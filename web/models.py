796698af4d6078e819cc733a4fa908fa9279cdb2
root <root@localhost.localdomain>
2022-05-06 04:16:43 +0430

    Income is here

--- web/models.py
+++ web/models.py
@@ -6,3 +6,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __unicode__(self):
+        return ("%d-%d" %(self.date, self.amount))
+    
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __unicode__(self):
+        return ('%d %d' %(self.date, self.amount))
